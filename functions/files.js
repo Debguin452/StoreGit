d500489c856e9f20ce4a2dd45c2394eab80513a3
Deb Guin <sukumarguin10@gmail.com>
2026-06-10 10:47:53 +0530

    Add files via upload

--- functions/files.js
+++ functions/files.js
@@ -1,5 +1,8 @@
 export async function onRequest({ request, env }) {
-  const url = new URL(request.url);
-  url.pathname = '/files.html';
-  return env.ASSETS.fetch(new Request(url.toString(), request));
+  if (env.ASSETS) {
+    const url = new URL(request.url);
+    url.pathname = '/files.html';
+    return env.ASSETS.fetch(url.toString());
+  }
+  return Response.redirect(new URL(request.url).origin + '/', 302);
 }
